3b1e1843630db5b71c8eb8f363a1e4d80147dc14
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-20 11:52:30 -0700

    rewrote function getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,8 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const TV_MAZE_SHOWS = "http://api.tvmaze.com/search/shows"
+//const TV_MAZE_EPISODES = `http://api.tvmaze.com/shows/${ }/episodes`
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,25 +14,20 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(q) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-
+  //take term and make request to TV show API for object
+  const show = await axios.get(TV_MAZE_SHOWS, {params: {q}});
+  const id = show.data[0].show.id;
+  const name = show.data[0].show.name;
+  const summary = show.data[0].show.summary;
+  const image = show.data[0].show.image.medium
   return [
     {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+      id,
+      name,
+      summary,
+      image
     }
   ]
 }
@@ -45,9 +42,9 @@ function populateShows(shows) {
     const $show = $(
         `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -56,7 +53,7 @@ function populateShows(shows) {
                Episodes
              </button>
            </div>
-         </div>  
+         </div>
        </div>
       `);
 
